58a129efebff47bc9735141da560cdc0fbe5ba4c
darya-loboca <dasha.lobova.it@gmail.com>
2026-05-12 10:32:06 +0300

    Task06 solved

--- Task06/logic.cpp
+++ Task06/logic.cpp
@@ -5,3 +5,38 @@
 // и реализовать функцию, которая сортирует часть элементов заданного вектора
 // в указанном диапазоне порядковых номеров либо по возрастанию, 
 // либо по убыванию.
+
+void sort_in_range(int* array, int size, int start, int end, bool type) {
+    if (array == nullptr || size <= 0) {
+        return;
+    }
+
+    if (start < 0 || start >= size || end < 0 || end >= size) {
+        return;
+    }
+
+    int actual_start = (start < end) ? start : end;
+    int actual_end = (start < end) ? end : start;
+
+    int* p_start = array + actual_start;
+    int* p_end = array + actual_end;
+
+    for (int* i = p_start; i < p_end; i++) {
+        int* extreme = i;
+
+        for (int* j = i + 1; j <= p_end; j++) {
+            if (type) { 
+                if (*j < *extreme) extreme = j;
+            }
+            else {    
+                if (*j > *extreme) extreme = j;
+            }
+        }
+
+        if (extreme != i) {
+            int temp = *i;
+            *i = *extreme;
+            *extreme = temp;
+        }
+    }
+}
