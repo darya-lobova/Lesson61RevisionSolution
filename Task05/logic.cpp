e3d53e9fae0e7b5cb47cc646587d5d0dbf72a911
darya-loboca <dasha.lobova.it@gmail.com>
2026-05-12 10:23:43 +0300

    Task05 solved

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -4,3 +4,37 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет сумму элементов вектора, расположенных
 // между первыми максимальным по модулю и минимальным по модулю элементами.
+
+int sum_between_first_min_max(int* array, int size) {
+    if (size <= 1 || array == nullptr) {
+        return 0;
+    }
+
+    int* max_ptr = array; 
+    int* min_ptr = array; 
+
+    for (int* p = array + 1; p < array + size; p++) {
+        
+        int cur_abs = (*p < 0) ? -(*p) : *p;
+        int max_abs = (*max_ptr < 0) ? -(*max_ptr) : *max_ptr;
+        int min_abs = (*min_ptr < 0) ? -(*min_ptr) : *min_ptr;
+
+        if (cur_abs > max_abs) {
+            max_ptr = p; 
+        }
+        if (cur_abs < min_abs) {
+            min_ptr = p; 
+        }
+    }
+
+    int* start = (min_ptr < max_ptr) ? min_ptr : max_ptr;
+    int* end = (min_ptr < max_ptr) ? max_ptr : min_ptr;
+
+    int sum = 0;
+  
+    for (int* p = start + 1; p < end; p++) {
+        sum += *p;
+    }
+
+    return sum;
+}
